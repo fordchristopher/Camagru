980fdb5b2e5d5517220e530a2f3df29880d137c9
Christopher Ford <chford@e1z2r1p13.42.us.org>
2019-09-08 16:02:13 -0700

    Final Client-side changes.  Going to move to school repository and test to turn in

--- camagru/src/global.js
+++ camagru/src/global.js
@@ -1,3 +1,3 @@
 //export const APIUrl = "http://localhost:8000";
-export const APIUrl = "http://localhost:8080";
+export const APIUrl = "http://localhost:8000";
 export const baseURL = "http://localhost:3000";
